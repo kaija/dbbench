1473911a648a0793acee256186fe519f5872db60
kevin k chang <kaija.chang.trend@gmail.com>
2015-07-29 10:24:03 +0800

    add couchbase connection

--- couchbase/routes/index.js
+++ couchbase/routes/index.js
@@ -1,16 +1,42 @@
 var express = require('express');
 var router = express.Router();
 var poolModule = require('generic-pool');
+var couchbase = require('couchbase');
+
+var db_username = 'Administrator';
+var db_password = '';
+var db_bucket = 'default';
+
+
+var cluster = new couchbase.Cluster('couchbase://127.0.0.1');
+var cm = cluster.manager(db_username, db_password);
+cm.listBuckets(function(err, list){
+  var bucketExist = false;
+  for (i in list){
+    if(list[i].name === db_bucket) {
+        bucketExist = true;
+      }
+  }
+  if(!bucketExist) {
+    console.log('Bucket not exist\n');
+  }
+});
+
 var pool = poolModule.Pool({
   name        : 'db',
   create      : function(callback) {
-    callback(null, new Object());
+    var cluster = new couchbase.Cluster('couchbase://127.0.0.1');
+    var bucket = cluster.openBucket(db_bucket);
+    callback(null, bucket);
   },
   destroy     : function(client) {
+    client.disconnect();
   },
   max         : 50,
   min         : 10
 });
+
+var counter = 0;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -19,8 +45,11 @@ router.get('/', function(req, res, next) {
 
 router.get('/create', function(req, res, next) {
   pool.acquire(function(err, client){
-    res.send("OK");
-    pool.release(client);
+    var id = new Date().getTime();
+    client.upsert(id.toString(), {id:id}, function(err, ret){
+      res.send(ret);
+      pool.release(client);
+    });
   });
 });
 
